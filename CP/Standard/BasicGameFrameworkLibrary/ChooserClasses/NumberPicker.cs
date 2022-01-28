8040f58b7a9c9e3843956ce87df26833be923241
Andy <musictopia2@hotmail.com>
2022-01-28 15:42:33 -0700

    Did partial class for number picker. Plus used attribute so i can start working on the source generation for control commands.

--- CP/Standard/BasicGameFrameworkLibrary/ChooserClasses/NumberPicker.cs
+++ CP/Standard/BasicGameFrameworkLibrary/ChooserClasses/NumberPicker.cs
@@ -1,12 +1,12 @@
 ﻿namespace BasicGameFrameworkLibrary.ChooserClasses;
-
-public class NumberPicker : SimpleControlObservable
+public partial class NumberPicker : SimpleControlObservable
 {
     private readonly ItemChooserClass<NumberModel> _privateChoose;
     public readonly BasicList<NumberModel> NumberList = new();
     public ControlCommand NumberPickedCommand { get; set; }
     public event ChangedNumberValueEventHandler? ChangedNumberValueAsync;
     public delegate Task ChangedNumberValueEventHandler(int Chosen);
+    [Command(EnumCommandCategory.Control)]
     private async Task ChooseNumberAsync(NumberModel piece)
     {
         SelectNumberValue(piece.NumberValue);
