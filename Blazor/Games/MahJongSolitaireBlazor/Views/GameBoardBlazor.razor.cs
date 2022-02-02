6bb20a32fdd9d4af0515e1aca0721628016ad809
Andy <musictopia2@hotmail.com>
2022-02-01 18:38:48 -0700

    Few minor bug fixes for mahjong solitaire.

--- Blazor/Games/MahJongSolitaireBlazor/Views/GameBoardBlazor.razor.cs
+++ Blazor/Games/MahJongSolitaireBlazor/Views/GameBoardBlazor.razor.cs
@@ -8,13 +8,13 @@ public partial class GameBoardBlazor
 {
     [Parameter]
     public BasicList<BoardInfo> BoardList { get; set; } = new();
-    private static TileGame GetTileKey(MahjongSolitaireTileInfo tile)
-    {
-        return new TileGame()
-        {
-            Deck = tile.Deck,
-            GameNumber = MahJongSolitaireMainViewModel.GameDrawing
-        };
-    }
+    //private static TileGame GetTileKey(MahjongSolitaireTileInfo tile)
+    //{
+    //    return new TileGame()
+    //    {
+    //        Deck = tile.Deck,
+    //        GameNumber = MahJongSolitaireMainViewModel.GameDrawing
+    //    };
+    //}
     private static string GetGameKey => $"MahjongGame{MahJongSolitaireMainViewModel.GameDrawing}";
 }
