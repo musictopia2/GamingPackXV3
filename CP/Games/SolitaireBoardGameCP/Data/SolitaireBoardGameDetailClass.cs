c4060497d5c1270a57fff95e106f7a4d39cffbdb
Andy <musictopia2@hotmail.com>
2022-02-09 08:12:11 -0700

    Removed autoresume for solitaire board game because it was causing blank screens.  Not worth investigating since its meant to play the entire game at once anyways.

--- CP/Games/SolitaireBoardGameCP/Data/SolitaireBoardGameDetailClass.cs
+++ CP/Games/SolitaireBoardGameCP/Data/SolitaireBoardGameDetailClass.cs
@@ -10,7 +10,7 @@ public class SolitaireBoardGameDetailClass : IGameInfo
     int IGameInfo.NoPlayers => 0;
     int IGameInfo.MinPlayers => 0;
     int IGameInfo.MaxPlayers => 0;
-    bool IGameInfo.CanAutoSave => true;
+    bool IGameInfo.CanAutoSave => false; //decided no more autoresume on this one.  somehow showed everything as blank (wrong).  not important on this game anyways.
     EnumSmallestSuggested IGameInfo.SmallestSuggestedSize => EnumSmallestSuggested.AnyDevice; //default to smallest but can change as needed.
     EnumSuggestedOrientation IGameInfo.SuggestedOrientation => EnumSuggestedOrientation.Portrait; //default to portrait but can change to what is needed.
 }
