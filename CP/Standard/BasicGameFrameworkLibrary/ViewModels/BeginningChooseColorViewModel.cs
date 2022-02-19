3b1f4782c8980d58db99a0fc339c7d3a1a7140a4
Andy <musictopia2@hotmail.com>
2022-02-19 14:59:49 -0700

    Fixes necessary in order to start creating the beginning choose color view.

--- CP/Standard/BasicGameFrameworkLibrary/ViewModels/BeginningChooseColorViewModel.cs
+++ CP/Standard/BasicGameFrameworkLibrary/ViewModels/BeginningChooseColorViewModel.cs
@@ -1,64 +1,52 @@
-﻿//using BasicGameFrameworkLibrary.ChooserClasses;
-//using BasicGameFrameworkLibrary.CommandClasses;
-//using BasicGameFrameworkLibrary.MultiplayerClasses.BasicPlayerClasses;
-//using BasicGameFrameworkLibrary.MultiplayerClasses.InterfacesForHelpers;
-//using BasicGameFrameworkLibrary.MultiplayerClasses.MiscHelpers;
-//using BasicGameFrameworkLibrary.ViewModelInterfaces;
-//using MessengingHelpers;
-//using MVVMFramework.ViewModels;
-//using System;
-//using System.Threading.Tasks; //most of the time, i will be using asyncs.
-//namespace BasicGameFrameworkLibrary.ViewModels
-//{
-//    public class BeginningChooseColorViewModel<E, P> : ScreenViewModel, IBlankGameVM, IBeginningColorViewModel, IDisposable
-//        where E : struct, Enum
-//        where P : class, IPlayerBoardGame<E>, new()
-//    {
-//        private readonly BeginningColorModel<E, P> _model;
-//        private readonly IBeginningColorProcesses<E> _processes;
-//        private bool _disposedValue;
-//        public string Turn { get; set; } = "";
-//        public string Instructions { get; set; } = "";
-//        public BeginningChooseColorViewModel(CommandContainer commandContainer,
-//            BeginningColorModel<E, P> model,
-//            IBeginningColorProcesses<E> processes,
-//            IEventAggregator aggregator
-//            ) : base(aggregator)
-//        {
-//            CommandContainer = commandContainer;
-//            _model = model;
-//            _processes = processes;
-//            _processes.SetInstructions = (x => Instructions = x);
-//            _processes.SetTurn = (x => Turn = x); //has to set delegates before init obviously.
-//            _model.ColorChooser.AutoSelectCategory = EnumAutoSelectCategory.AutoEvent;
-//            _model.ColorChooser.ItemClickedAsync += ColorChooser_ItemClickedAsync;
-//        }
-//        public BoardGamesColorPicker<E, P> GetColorPicker => _model.ColorChooser;
-//        protected override Task ActivateAsync()
-//        {
-//            return _processes.InitAsync();
-//        }
-//        private Task ColorChooser_ItemClickedAsync(E piece)
-//        {
-//            return _processes.ChoseColorAsync(piece);
-//        }
-//        public CommandContainer CommandContainer { get; set; }
-//        protected virtual void Dispose(bool disposing)
-//        {
-//            if (!_disposedValue)
-//            {
-//                if (disposing)
-//                {
-//                    _model.ColorChooser.ItemClickedAsync -= ColorChooser_ItemClickedAsync;
-//                }
-//                _disposedValue = true;
-//            }
-//        }
-//        public void Dispose()
-//        {
-//            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-//            Dispose(disposing: true);
-//            GC.SuppressFinalize(this);
-//        }
-//    }
-//}
+﻿namespace BasicGameFrameworkLibrary.ViewModels;
+public class BeginningChooseColorViewModel<E, P> : ScreenViewModel, IBlankGameVM, IBeginningColorViewModel, IDisposable
+    where E : struct, IFastEnumColorList<E>
+    where P : class, IPlayerBoardGame<E>, new()
+{
+    private readonly BeginningColorModel<E, P> _model;
+    private readonly IBeginningColorProcesses<E> _processes;
+    private bool _disposedValue;
+    public string Turn { get; set; } = "";
+    public string Instructions { get; set; } = "";
+    public BeginningChooseColorViewModel(CommandContainer commandContainer,
+        BeginningColorModel<E, P> model,
+        IBeginningColorProcesses<E> processes,
+        IEventAggregator aggregator
+        ) : base(aggregator)
+    {
+        CommandContainer = commandContainer;
+        _model = model;
+        _processes = processes;
+        _processes.SetInstructions = (x => Instructions = x);
+        _processes.SetTurn = (x => Turn = x); //has to set delegates before init obviously.
+        _model.ColorChooser.AutoSelectCategory = EnumAutoSelectCategory.AutoEvent;
+        _model.ColorChooser.ItemClickedAsync += ColorChooser_ItemClickedAsync;
+    }
+    public BoardGamesColorPicker<E, P> GetColorPicker => _model.ColorChooser;
+    protected override Task ActivateAsync()
+    {
+        return _processes.InitAsync();
+    }
+    private Task ColorChooser_ItemClickedAsync(E piece)
+    {
+        return _processes.ChoseColorAsync(piece);
+    }
+    public CommandContainer CommandContainer { get; set; }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _model.ColorChooser.ItemClickedAsync -= ColorChooser_ItemClickedAsync;
+            }
+            _disposedValue = true;
+        }
+    }
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
