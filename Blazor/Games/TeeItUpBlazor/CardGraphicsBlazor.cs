cc28aa0b79dc2de08eecc3671c679e2564dc87cd
Andy <musictopia2@hotmail.com>
2022-03-02 16:00:31 -0700

    Took out text for back of card for tee it up because of svg error.

--- Blazor/Games/TeeItUpBlazor/CardGraphicsBlazor.cs
+++ Blazor/Games/TeeItUpBlazor/CardGraphicsBlazor.cs
@@ -33,17 +33,19 @@ public class CardGraphicsBlazor : BaseDeckGraphics<TeeItUpCardInformation>
     }
     protected override void DrawBacks()
     {
-        RectangleF firstRect;
-        RectangleF secondRect;
-        RectangleF thirdRect;
-        firstRect = new RectangleF(2, 2, 74, 33);
-        secondRect = new RectangleF(2, 35, 74, 33);
-        thirdRect = new RectangleF(2, 68, 74, 33);
-        var fontSize = firstRect.Height * 0.6f; // can be adjusted
-        string color = cc.White;
-        DrawText("Tee", firstRect, color, fontSize, true);
-        DrawText("It", secondRect, color, fontSize, true);
-        DrawText("Up", thirdRect, color, fontSize, true);
+        //could not have text for backs of cards because of svg bug (trying to remove element).
+        return;
+        //RectangleF firstRect;
+        //RectangleF secondRect;
+        //RectangleF thirdRect;
+        //firstRect = new RectangleF(2, 2, 74, 33);
+        //secondRect = new RectangleF(2, 35, 74, 33);
+        //thirdRect = new RectangleF(2, 68, 74, 33);
+        //var fontSize = firstRect.Height * 0.6f; // can be adjusted
+        //string color = cc.White;
+        //DrawText("Tee", firstRect, color, fontSize, true);
+        //DrawText("It", secondRect, color, fontSize, true);
+        //DrawText("Up", thirdRect, color, fontSize, true);
     }
     protected override void DrawImage()
     {
