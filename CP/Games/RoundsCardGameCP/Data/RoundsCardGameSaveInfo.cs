005fa02b0f2ee5175424ec98065e99be533ca797
Andy Walter <musictopia2@hotmail.com>
2022-03-11 09:11:28 -0700

    Fixed autoresume bug on rounds card game.

--- CP/Games/RoundsCardGameCP/Data/RoundsCardGameSaveInfo.cs
+++ CP/Games/RoundsCardGameCP/Data/RoundsCardGameSaveInfo.cs
@@ -3,5 +3,5 @@ namespace RoundsCardGameCP.Data;
 public class RoundsCardGameSaveInfo : BasicSavedTrickGamesClass<EnumSuitList, RoundsCardGameCardInformation, RoundsCardGamePlayerItem>, IMappable, ISaveInfo
 {
     public int PartRound { get; set; } //from 1 to 5.
-    public DeckRegularDict<RoundsCardGameCardInformation> CardList = new();
+    public DeckRegularDict<RoundsCardGameCardInformation> CardList { get; set; } = new();
 }
