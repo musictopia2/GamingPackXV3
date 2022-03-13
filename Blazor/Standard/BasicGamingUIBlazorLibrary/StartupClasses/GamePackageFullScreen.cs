0c9322601b6136dfecdf75de310103ce874087ca
Andy Walter <musictopia2@hotmail.com>
2022-03-13 08:12:17 -0700

    Added a full screen for game package. Also, some fixes to attempt to make scrolling work when its needed. Especially on choosing how many players.

---
+++ Blazor/Standard/BasicGamingUIBlazorLibrary/StartupClasses/GamePackageFullScreen.cs
@@ -0,0 +1,6 @@
+﻿namespace BasicGamingUIBlazorLibrary.StartupClasses;
+public class GamePackageFullScreen : FullPageComponentBase
+{
+    public override string BackgroundColor => cc.Navy.ToWebColor();
+    public override string TextColor => cc.Aqua.ToWebColor();
+}
